2eda391cc225284504b888590e034389fe8b455f
qDeveloper <cihan300@gmail.com>
2021-05-19 14:40:45 +0300

    orm elemanları ve fonksyionlar oluşturuldu

---
+++ process.js
@@ -0,0 +1,12 @@
+let hesaplaBtn = document.querySelector("#hesapla")
+let cekTopTut = document.querySelector("#cekTopTut")
+let vade = document.querySelector("#vade")
+let faiz = document.querySelector("#faiz")
+let bsmv = document.querySelector("#BSMV")
+let kkdf = document.querySelector("#KKDF")
+
+hesaplaBtn.addEventListener("click",hesaplaFunc)
+
+function hesaplaFunc(){
+
+}
