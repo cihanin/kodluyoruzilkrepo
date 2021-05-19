f15538c7e84d51692a52107ce025509b12c2fa11
qDeveloper <cihan300@gmail.com>
2021-05-19 16:47:05 +0300

    Form işlemleri yapıldı ve değerler hesaplanarak taksit sayısı kadar dögüye sokuldu

--- process.js
+++ process.js
@@ -1,12 +1,50 @@
 let hesaplaBtn = document.querySelector("#hesapla")
-let cekTopTut = document.querySelector("#cekTopTut")
-let vade = document.querySelector("#vade")
-let faiz = document.querySelector("#faiz")
-let bsmv = document.querySelector("#BSMV")
-let kkdf = document.querySelector("#KKDF")
+
 
 hesaplaBtn.addEventListener("click",hesaplaFunc)
 
 function hesaplaFunc(){
+    let cekTopTut = document.querySelector("#cekTopTut")
+    let vade = document.querySelector("#vade")
+    let faiz = document.querySelector("#faiz")
+    let bsmv = document.querySelector("#BSMV")
+    let kkdf = document.querySelector("#KKDF")
+    let anaPara = document.querySelector("#anaPara")
+    let ayOdeMik = document.querySelector("#aylikOdemeMiktari")
+    let topMal = document.querySelector("#toplamMaliyet")
+    let topFaiz = document.querySelector("#toplamFaiz")
+    let brutFaiz = document.querySelector("#brutFaiz")
+    var takBastir = document.querySelector("#takBastir")
+    let takSatir =""
 
+    let cekTopTutx = Number(cekTopTut.value)
+    let vadex = Number(vade.value)
+    let faizx = Number(faiz.value)
+    let bsmvx = Number(bsmv.value)
+    let kkdfx = Number(kkdf.value)
+
+    let aylikFaiz = ((faizx+(faizx*bsmvx/100)+(faizx*kkdfx/100))/100)
+    anaPara.value = cekTopTutx
+    aylikTaksit = ((((1+aylikFaiz)**vadex)*aylikFaiz)/(((1+aylikFaiz)**vadex)-1))*cekTopTutx
+    ayOdeMik.value = aylikTaksit.toFixed(2)
+    topMal.value = (vadex * aylikTaksit).toFixed(2)
+    topFaiz.value = ((vadex * aylikTaksit) - cekTopTutx).toFixed(2)
+    brutFaiz.value = aylikFaiz * 100
+
+    for(i = 0; i < vadex; i++){
+        takSatir +=`
+            <tr>
+                <td>asd</td>
+                <td>asd</td>
+                <td>asd</td>
+                <td>asd</td>
+                <td>asd</td>
+                <td>asd</td>
+                <td>asd</td>
+                <td>asd</td>
+            </tr>
+        `
+    }
+    //console.log(takSatir)
+    takBastir.innerHTML = takSatir
 }
