bee79d447910d11d5251a87fbcfecf1f9c84c030
qDeveloper <cihan300@gmail.com>
2021-05-21 00:31:48 +0300

    Program doğru bir şekilde çalışıyor (Kod temizlenecek) ve css ile arayüz geliştirilecek

--- process.js
+++ process.js
@@ -16,6 +16,15 @@ function hesaplaFunc(){
     let brutFaiz = document.querySelector("#brutFaiz")
     var takBastir = document.querySelector("#takBastir")
     let takSatir =""
+    let kkdfA = []
+    let bsmvA = []
+    let burutFaizA= []
+    let kalanAnaPara = []
+    let anaParaTaksit = []
+    let netFaiz = []
+    let z = 0
+
+
 
     let cekTopTutx = Number(cekTopTut.value)
     let vadex = Number(vade.value)
@@ -23,25 +32,51 @@ function hesaplaFunc(){
     let bsmvx = Number(bsmv.value)
     let kkdfx = Number(kkdf.value)
 
+    
     let aylikFaiz = ((faizx+(faizx*bsmvx/100)+(faizx*kkdfx/100))/100)
     anaPara.value = cekTopTutx
-    aylikTaksit = ((((1+aylikFaiz)**vadex)*aylikFaiz)/(((1+aylikFaiz)**vadex)-1))*cekTopTutx
-    ayOdeMik.value = aylikTaksit.toFixed(2)
-    topMal.value = (vadex * aylikTaksit).toFixed(2)
-    topFaiz.value = ((vadex * aylikTaksit) - cekTopTutx).toFixed(2)
+    aylikTaksit = (((((1+aylikFaiz)**vadex)*aylikFaiz)/(((1+aylikFaiz)**vadex)-1))*cekTopTutx)
+    ayOdeMik.value = aylikTaksit
+    topMal.value = (vadex * aylikTaksit)
+    topFaiz.value = ((vadex * aylikTaksit) - cekTopTutx)
     brutFaiz.value = aylikFaiz * 100
+    //netFaiz = (i==0)?((cekTopTutx*faizx)/100):kalanAnaPara-ayOdeMik.value
 
-    for(i = 0; i < vadex; i++){
+    for(i = 0; i <= vadex-1; i++){
+        
+        if(i<=0){
+            netFaiz[i] = ((cekTopTutx * faizx)/100)
+            kkdfA[i] = netFaiz[i]*(kkdfx/100)
+            bsmvA[i] = netFaiz[i]*(bsmvx/100)
+            burutFaizA[i] = netFaiz[i] + bsmvA[i] + kkdfA[i]
+            anaParaTaksit[i] = aylikTaksit-burutFaizA[i]
+            kalanAnaPara[i] = (cekTopTutx-anaParaTaksit)
+            console.log(i+"-i")
+        }else{
+            z=i-1
+            console.log(i+"-i"+z+"-z")
+            netFaiz[i] = ((kalanAnaPara[i-1] * faizx)/100)
+            console.log(netFaiz[i]+" - "+kalanAnaPara[i-1])
+            kkdfA[i] = netFaiz[i]*(kkdfx/100)
+            bsmvA[i] = netFaiz[i]*(bsmvx/100)
+            burutFaizA[i] = netFaiz[i] + bsmvA[i] + kkdfA[i]
+            anaParaTaksit[i] = aylikTaksit-burutFaizA[i]
+            //Ödeme bittiği için kontrole sok vade sonu geldiyse hesaplama sıfır bas.
+            if(i==vadex-1){
+                kalanAnaPara[i] = 0
+            }else{
+                kalanAnaPara[i] = kalanAnaPara[i-1]-anaParaTaksit[i]
+            }
+        }
         takSatir +=`
             <tr>
-                <td>asd</td>
-                <td>asd</td>
-                <td>asd</td>
-                <td>asd</td>
-                <td>asd</td>
-                <td>asd</td>
-                <td>asd</td>
-                <td>asd</td>
+                <td>${i+1}</td>
+                <td>${anaParaTaksit[i]}</td>
+                <td>${netFaiz[i]}</td>
+                <td>${kkdfA[i]}</td>
+                <td>${bsmvA[i]}</td>
+                <td>${burutFaizA[i]}</td>
+                <td>${kalanAnaPara[i]}</td>
             </tr>
         `
     }
